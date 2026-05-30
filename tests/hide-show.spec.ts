3a7f1563db984b656af58dd1ac12a64b250af698
Peder Nylander <peder@pedernylander.se>
2026-05-30 09:59:14 +0200

    Adding a test.describe so we can add more tests here later on

--- tests/hide-show.spec.ts
+++ tests/hide-show.spec.ts
@@ -4,44 +4,46 @@ import { test, expect } from '@playwright/test';
 const BASE_URL = 'http://localhost:3000';
 test.use({ storageState: 'storageState.json' });
 
-test('Hide/Show Todos E2E', async ({ page }) => {
-    await page.goto(BASE_URL);
-    // Go to admin page
-    await page.getByTestId('admin-link').click();
+test.describe('Hide/Show Todos E2E', () => {
 
-    // Change number of todos to load to 10
-    await page.getByTestId('admin-link-settings').click();
+    test('should hide completed todos when clicked and scrolling', async ({ page }) => {
+        await page.goto(BASE_URL);
+        // Go to admin page
+        await page.getByTestId('admin-link').click();
 
-    // Get number of todos to load before change
-    // @todo we need to put back this value after test. 
-    const loadLimitInput = 
-        await page.locator('[id="App::todos--defaultLoadLimit"]').inputValue();
+        // Change number of todos to load to 10
+        await page.getByTestId('admin-link-settings').click();
 
-    const newLoadLimit = 10;   
-    if(loadLimitInput !== newLoadLimit.toString()) {
-        await page.locator('[id="App::todos--defaultLoadLimit"]').fill(newLoadLimit.toString());
-        await page.getByTestId('save-setting-App::todos').click();
+        // Get number of todos to load before change
+        // @todo we need to put back this value after test. 
+        const loadLimitInput = 
+            await page.locator('[id="App::todos--defaultLoadLimit"]').inputValue();
+
+        const newLoadLimit = 10;   
+        if(loadLimitInput !== newLoadLimit.toString()) {
+            await page.locator('[id="App::todos--defaultLoadLimit"]').fill(newLoadLimit.toString());
+            await page.getByTestId('save-setting-App::todos').click();
+            await page.waitForTimeout(500);
+        }
+
+        
+        // Go back to todos page
+        await page.getByTestId('admin-link-todos').click();
         await page.waitForTimeout(500);
-    }
 
+        // Click "Hide completed" button (because default is to show completed todos)
+        await page.getByTestId('toggleShowCompleted').click();
     
-    // Go back to todos page
-    await page.getByTestId('admin-link-todos').click();
-    await page.waitForTimeout(500);
+        while (
+            !await page.getByText('All todos loaded').isVisible() &&
+            !await page.getByTestId(/^completed-/).isVisible()
+        ) {
+            await page.mouse.wheel(0, 500);
+            await page.waitForTimeout(300);
+        }
 
-    // Click "Hide completed" button (because default is to show completed todos)
-    await page.getByTestId('toggleShowCompleted').click();
-   
-    while (
-        !await page.getByText('All todos loaded').isVisible() &&
-        !await page.getByTestId(/^completed-/).isVisible()
-    ) {
-        await page.mouse.wheel(0, 500);
-        await page.waitForTimeout(300);
-    }
-
-    await expect(page.getByTestId(/^completed-/)).toHaveCount(0);
-
+        await expect(page.getByTestId(/^completed-/)).toHaveCount(0);
+    });
 
 });    
 
