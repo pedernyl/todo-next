6546f8d814879da97ed50ef3ab7be939bc9531ad
Peder Nylander <42877724+pedernyl@users.noreply.github.com>
2026-07-06 11:25:19 +0200

    Database changes for Category (#91)
    * Adding stored procedure for alter table Category to add columns deleted_timestamp and deleted_by
    
    * Adding adminUpdate file so we can update our database calling the stored procedure that adds soft delete columns for table Category
    
    * Removed alias import from updates files because we dont have that in our vitest config. Also added revoke to sql stored procedure

--- src/lib/adminUpdates/updates/registry.generated.ts
+++ src/lib/adminUpdates/updates/registry.generated.ts
@@ -2,62 +2,67 @@
 // AUTO-GENERATED FILE. DO NOT EDIT.
 // Run: npm run generate:admin-updates
 
-import * as update_addUsersIsAdminAndSeedFromAllowedUsers_1777381562_0 from "./addUsersIsAdminAndSeedFromAllowedUsers_1777381562";
-import * as update_alterSortIndexToInteger_1778659078_1 from "./alterSortIndexToInteger_1778659078";
-import * as update_capitalizeSettingsTypeFirstChar_1778225819_2 from "./capitalizeSettingsTypeFirstChar_1778225819";
-import * as update_ensureSettingsTableAuditColumns_1777470000_3 from "./ensureSettingsTableAuditColumns_1777470000";
-import * as update_normalizeNegativeTodoSortIndexToNull_1777482000_4 from "./normalizeNegativeTodoSortIndexToNull_1777482000";
-import * as update_reassignSortIndexDescendingWithGaps_1778658241_5 from "./reassignSortIndexDescendingWithGaps_1778658241";
-import * as update_reindexTodoSortIndexPerScope_1777482600_6 from "./reindexTodoSortIndexPerScope_1777482600";
-import * as update_renameTodosTableToTodos_1777392359_7 from "./renameTodosTableToTodos_1777392359";
-import * as update_renameUserTableToUsers_1777361949_8 from "./renameUserTableToUsers_1777361949";
-import * as update_replaceInsertTodoAtTop_1778661608_9 from "./replaceInsertTodoAtTop_1778661608";
-import * as update_setTodoSortIndexToMinusOne_1776152030_10 from "./setTodoSortIndexToMinusOne_1776152030";
+import * as update_add_category_soft_delete_columns_1783318540_0 from "./add_category_soft_delete_columns_1783318540";
+import * as update_addUsersIsAdminAndSeedFromAllowedUsers_1777381562_1 from "./addUsersIsAdminAndSeedFromAllowedUsers_1777381562";
+import * as update_alterSortIndexToInteger_1778659078_2 from "./alterSortIndexToInteger_1778659078";
+import * as update_capitalizeSettingsTypeFirstChar_1778225819_3 from "./capitalizeSettingsTypeFirstChar_1778225819";
+import * as update_ensureSettingsTableAuditColumns_1777470000_4 from "./ensureSettingsTableAuditColumns_1777470000";
+import * as update_normalizeNegativeTodoSortIndexToNull_1777482000_5 from "./normalizeNegativeTodoSortIndexToNull_1777482000";
+import * as update_reassignSortIndexDescendingWithGaps_1778658241_6 from "./reassignSortIndexDescendingWithGaps_1778658241";
+import * as update_reindexTodoSortIndexPerScope_1777482600_7 from "./reindexTodoSortIndexPerScope_1777482600";
+import * as update_renameTodosTableToTodos_1777392359_8 from "./renameTodosTableToTodos_1777392359";
+import * as update_renameUserTableToUsers_1777361949_9 from "./renameUserTableToUsers_1777361949";
+import * as update_replaceInsertTodoAtTop_1778661608_10 from "./replaceInsertTodoAtTop_1778661608";
+import * as update_setTodoSortIndexToMinusOne_1776152030_11 from "./setTodoSortIndexToMinusOne_1776152030";
 import type { RegisteredAdminUpdate } from "./registry";
 
 export const adminUpdateRegistry: RegisteredAdminUpdate[] = [
   {
+    fileName: "add_category_soft_delete_columns_1783318540.ts",
+    module: update_add_category_soft_delete_columns_1783318540_0,
+  },
+  {
     fileName: "addUsersIsAdminAndSeedFromAllowedUsers_1777381562.ts",
-    module: update_addUsersIsAdminAndSeedFromAllowedUsers_1777381562_0,
+    module: update_addUsersIsAdminAndSeedFromAllowedUsers_1777381562_1,
   },
   {
     fileName: "alterSortIndexToInteger_1778659078.ts",
-    module: update_alterSortIndexToInteger_1778659078_1,
+    module: update_alterSortIndexToInteger_1778659078_2,
   },
   {
     fileName: "capitalizeSettingsTypeFirstChar_1778225819.ts",
-    module: update_capitalizeSettingsTypeFirstChar_1778225819_2,
+    module: update_capitalizeSettingsTypeFirstChar_1778225819_3,
   },
   {
     fileName: "ensureSettingsTableAuditColumns_1777470000.ts",
-    module: update_ensureSettingsTableAuditColumns_1777470000_3,
+    module: update_ensureSettingsTableAuditColumns_1777470000_4,
   },
   {
     fileName: "normalizeNegativeTodoSortIndexToNull_1777482000.ts",
-    module: update_normalizeNegativeTodoSortIndexToNull_1777482000_4,
+    module: update_normalizeNegativeTodoSortIndexToNull_1777482000_5,
   },
   {
     fileName: "reassignSortIndexDescendingWithGaps_1778658241.ts",
-    module: update_reassignSortIndexDescendingWithGaps_1778658241_5,
+    module: update_reassignSortIndexDescendingWithGaps_1778658241_6,
   },
   {
     fileName: "reindexTodoSortIndexPerScope_1777482600.ts",
-    module: update_reindexTodoSortIndexPerScope_1777482600_6,
+    module: update_reindexTodoSortIndexPerScope_1777482600_7,
   },
   {
     fileName: "renameTodosTableToTodos_1777392359.ts",
-    module: update_renameTodosTableToTodos_1777392359_7,
+    module: update_renameTodosTableToTodos_1777392359_8,
   },
   {
     fileName: "renameUserTableToUsers_1777361949.ts",
-    module: update_renameUserTableToUsers_1777361949_8,
+    module: update_renameUserTableToUsers_1777361949_9,
   },
   {
     fileName: "replaceInsertTodoAtTop_1778661608.ts",
-    module: update_replaceInsertTodoAtTop_1778661608_9,
+    module: update_replaceInsertTodoAtTop_1778661608_10,
   },
   {
     fileName: "setTodoSortIndexToMinusOne_1776152030.ts",
-    module: update_setTodoSortIndexToMinusOne_1776152030_10,
+    module: update_setTodoSortIndexToMinusOne_1776152030_11,
   },
 ];
