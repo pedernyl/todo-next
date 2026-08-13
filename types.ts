1f71c6f611758c448a8d3256ed482dbd48ab3a3c
Peder Nylander <peder@pedernylander.se>
2026-08-13 10:14:54 +0200

    Adding constants for messages in userServiceResponse

--- types.ts
+++ types.ts
@@ -1,3 +1,6 @@
+import { API_MESSAGES } from "./src/constants/api/apiMessages";
+
+
 // Type definition for a Todo item
 export interface Todo {
   id: string;
@@ -12,3 +15,61 @@ export interface Todo {
   deleted_timestamp?: number | null;
   deleted_by?: number | null;
 }
+
+// API responses 
+export type httpStatusCode = 200 | 400 | 401 | 403 | 404 | 500;
+
+export interface ApiResponse<T> {
+  status: httpStatusCode;
+  data?: T;
+  code?: string | number;
+  error?: string;
+  ok?: boolean;
+  message?: string;
+}
+
+// Type definition for userServiceResponse
+type userServiceResponseData = null | number;
+
+export interface userServiceResponse{
+  [httpStatusCode:number]: ApiResponse<userServiceResponseData>;
+}
+
+export const userServiceResponse: userServiceResponse = {
+  200: { 
+    status: 200, 
+    ok: true 
+  },
+  400: { 
+    status: 400, 
+    ok: false, 
+    message: API_MESSAGES.USER.MISSING_EMAIL,
+    data: null
+  },
+  401: { 
+    status: 401, 
+    ok: false,  
+    message: API_MESSAGES.COMMON.UNAUTHORIZED, 
+    data: null 
+  },
+  403: { 
+    status: 403, 
+    ok: false, 
+    message: API_MESSAGES.COMMON.FORBIDDEN,
+    data: null
+  },
+  404: { 
+    status: 404, 
+    ok: false, 
+    message: API_MESSAGES.USER.USER_NOT_FOUND,
+    data: null
+  },
+  500: { 
+    status: 500, 
+    ok: false, 
+    message: API_MESSAGES.COMMON.INTERNAL_SERVER_ERROR,
+    data: null
+  },
+};
+
+
