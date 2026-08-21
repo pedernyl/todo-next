b884dddfeaf863c1b340c4032217bdd8cd590d76
Peder Nylander <peder@pedernylander.se>
2026-08-21 11:30:09 +0200

    Removed UserIdContext

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
-import { UserIdProvider } from "../context/UserIdContext";
 import { GlobalBlockingLoaderProvider } from "../context/GlobalBlockingLoaderContext";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
       <GlobalBlockingLoaderProvider>
-        <UserIdProvider>{children}</UserIdProvider>
+        {children}
       </GlobalBlockingLoaderProvider>
     </SessionProvider>
   );
