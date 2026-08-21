0abff2af7f308296557a5ed02911208714dabfd2
Peder Nylander <peder@pedernylander.se>
2026-08-21 10:58:19 +0200

    Added isAdmin and id from database to session.user and token.id

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: number;
+      isAdmin?: boolean;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    isAdmin?: boolean;
+  }
+}
