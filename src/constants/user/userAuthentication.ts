75cd6987163349a915c46c2c50bc80696c972c7c
Peder Nylander <peder@pedernylander.se>
2026-08-24 11:26:35 +0200

    Added function refreshUserSession that adds user.id, user.isAdmin to session.user if missing. When we merge this into main branch on other installations user.id and user.isAdmin doesnt exist and we need to regenerate the session.user

--- src/constants/user/userAuthentication.ts
+++ src/constants/user/userAuthentication.ts
@@ -3,5 +3,6 @@ export const USER_AUTHENTICATION_MESSAGES = {
         MISSING_EMAIL: "User email missing",
         USER_NOT_FOUND: "User not found",
         USER_ID_LOOKUP_FAILED: "User ID lookup failed",
+        USER_UNAUTHENTICATED: "User is not authenticated",
   },
 } as const;
