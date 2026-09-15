03c0618ba525a4e54afdf1dbf47fad106a522100
Peder Nylander <peder@pedernylander.se>
2026-09-15 13:20:15 +0200

    Added shouldUseInitialTodosRef for checking if todos is in initial state to avoid that we first fetch todos from page and then fetch todos againg from TodoPageClient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,8 @@ export default async function Home() {
   
   const policy = await getTodoLoadPolicy();
   const effectiveLimit = computeEffectiveLimit(policy);
-  const todos = await getTodos(true, undefined, effectiveLimit);
+  const todos = await getTodos(false, undefined, effectiveLimit);
+  console.log('todos page.tsx', todos);
   const initialCategories = await getCategories({ 
     ownerId: userId, 
     completed: false, 
@@ -70,6 +71,7 @@ export default async function Home() {
       <TodoPageClient 
         initialTodos={todos}
         initialCategories={initialCategories} 
+        defaultPageSize={effectiveLimit}
        />
     </div>
   );
